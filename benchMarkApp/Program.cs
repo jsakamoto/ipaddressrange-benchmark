2a6563fb06049d0d83ec0dda1f80359d625dfeeb
jsakamoto <junichi.jp@gmail.com>
2015-10-16 21:06:27 +0900

    Optimize to speed benchmark - using Stopwatch class and report it in console.

--- benchMarkApp/Program.cs
+++ benchMarkApp/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using NetTools;
 
@@ -14,10 +16,16 @@ class Program
         var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ipv4ranges_dummy.txt");
         var ipv4RangeTexts = File.ReadLines(path);
         var index = 0;
+
+        Thread.Sleep(1000);
+        var stopwatch = Stopwatch.StartNew();
         foreach (var ipv4RangeText in ipv4RangeTexts)
         {
             stock[index] = IPAddressRange.Parse(ipv4RangeText);
             index++;
         }
+        stopwatch.Stop();
+
+        Console.WriteLine(stopwatch.ElapsedMilliseconds);
     }
 }
